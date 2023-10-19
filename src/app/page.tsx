fbc372802a872f581041704d299059f4255b1e8e
QuiLion7 <quilbrub@gmail.com>
2023-10-19 01:09:25 -0300

    chore: login and logout working, add avatar and separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home2</div>;
+  const { data } = useSession();
+  return <div>{data?.user?.name}</div>;
 }
