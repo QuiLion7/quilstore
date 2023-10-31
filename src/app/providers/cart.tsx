24b0d6ef402de6e7163b1088947110be2564289f
QuiLion7 <quilbrub@gmail.com>
2023-10-31 05:31:45 -0300

    feat: ensure product can be added to cart

--- src/app/providers/cart.tsx
+++ src/app/providers/cart.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { Product } from "@prisma/client";
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends Product {
   quantity: number;
 }
 
@@ -12,20 +12,29 @@ interface ICardContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
 }
 
-const CartContext = createContext<ICardContext>({
+export const CartContext = createContext<ICardContext>({
   products: [],
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
+  addProductToCart: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const addProductToCart = (product: CartProduct) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
   return (
     <CartContext.Provider
       value={{
-        products: [],
+        products,
+        addProductToCart,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
