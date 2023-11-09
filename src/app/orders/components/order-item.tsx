b70670fe71d9251e209c662c2acd12e5623928f0
QuiLion7 <quilbrub@gmail.com>
2023-11-09 06:54:11 -0300

    feat: add totals to order item and ensure order status is properly formatted

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -8,6 +8,10 @@ import { Card } from "@/components/ui/card";
 import { Prisma } from "@prisma/client";
 import { format } from "date-fns";
 import OrderProductItem from "./order-product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { useMemo } from "react";
+import { Separator } from "@/components/ui/separator";
+import { getOrderStatus } from "./helpers/status";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -22,33 +26,80 @@ interface OrderItemProps {
 }
 
 const OrderItem = ({ order }: OrderItemProps) => {
+  const subtotal = useMemo(() => {
+    return order.orderProducts.reduce((acc, orderProduct) => {
+      return (
+        acc + Number(orderProduct.product.basePrice) * orderProduct.quantity
+      );
+    }, 0);
+  }, [order.orderProducts]);
+
+  const total = useMemo(() => {
+    return order.orderProducts.reduce((acc, product) => {
+      const productWithTotalPrice = computeProductTotalPrice(product.product);
+
+      return acc + productWithTotalPrice.totalPrice * product.quantity;
+    }, 0);
+  }, [order.orderProducts]);
+
+  const totalDiscounts = subtotal - total;
+
   return (
     <Card className="px-5">
       <Accordion type="single" className="w-full" collapsible>
         <AccordionItem value={order.id}>
           <AccordionTrigger>
-            <div className="flex flex-col gap-1 text-left">
-              Pedido com {order.orderProducts.length} produto(s)
+            <div className="flex w-full text-left">
+              <div className="flex flex-1 flex-col gap-1 text-left">
+                <p className="text-sm font-bold uppercase lg:text-base">
+                  Pedido com {order.orderProducts.length} produto(s)
+                </p>
+                <span className="text-xs opacity-60">
+                  Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
+                </span>
+              </div>
+
+              <div className="hidden flex-1 font-bold lg:block">
+                <p className="text-xs lg:text-sm">Status</p>
+                <p className="text-xs text-[#8162FF] lg:text-sm">
+                  {getOrderStatus(order.status)}
+                </p>
+              </div>
+
+              <div className="hidden flex-1 lg:block">
+                <p className="text-xs font-bold lg:text-sm ">Data</p>
+                <p className="text-xs opacity-60 lg:text-sm">
+                  {format(order.createdAt, "d/MM/y")}
+                </p>
+              </div>
+
+              <div className="hidden flex-1 lg:block">
+                <p className="text-xs font-bold lg:text-sm">Pagamento</p>
+                <p className="text-xs opacity-60 lg:text-sm">Cartão</p>
+              </div>
             </div>
           </AccordionTrigger>
+
           <AccordionContent>
             <div className="flex flex-col gap-4">
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between lg:hidden">
                 <div className="font-bold">
-                  <span>Status</span>
-                  <span className="text[#8162FF]">{order.status}</span>
+                  <p className="text-xs lg:text-sm">Status</p>
+                  <p className="text-xs text-[#8162FF] lg:text-sm">
+                    {getOrderStatus(order.status)}
+                  </p>
                 </div>
 
                 <div>
-                  <p className="font-bold">Data</p>
-                  <p className="opacity-60">
+                  <p className="text-xs font-bold lg:text-sm">Data</p>
+                  <p className="text-xs opacity-60 lg:text-sm">
                     {format(order.createdAt, "d/MM/y")}
                   </p>
                 </div>
 
                 <div>
-                  <p className="font-bold">Data</p>
-                  <p className="opacity-60">Cartão</p>
+                  <p className="text-xs font-bold lg:text-sm">Pagamento</p>
+                  <p className="text-xs opacity-60 lg:text-sm">Cartão</p>
                 </div>
               </div>
 
@@ -58,6 +109,36 @@ const OrderItem = ({ order }: OrderItemProps) => {
                   orderProduct={orderProduct}
                 />
               ))}
+
+              <div className="flex w-full flex-col gap-1 text-xs">
+                <Separator />
+
+                <div className="flex w-full justify-between py-3 lg:text-sm">
+                  <p>Subtotal</p>
+                  <p>R$ {subtotal.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3 lg:text-sm">
+                  <p>Entrega</p>
+                  <p>GRÁTIS</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3 lg:text-sm">
+                  <p>Descontos</p>
+                  <p>-R$ {totalDiscounts.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3 text-sm font-bold lg:text-base">
+                  <p>Total</p>
+                  <p>R$ {total.toFixed(2)}</p>
+                </div>
+              </div>
             </div>
           </AccordionContent>
         </AccordionItem>
